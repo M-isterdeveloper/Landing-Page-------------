4ffb217845280883b2c48364a118c62eb2ecd0be
M-sterdeveloper <gubkinivan816@gmail.com>
2021-12-25 21:46:36 +0700

    Залил учебный проект

---
+++ src/js/modules/forms.js
@@ -0,0 +1,71 @@
+import checkNumInputs from './checkNumInputs';
+
+const forms = (state) => {
+    const form = document.querySelectorAll('form'),
+          inputs = document.querySelectorAll('input');
+
+    checkNumInputs('input[name="user_phone"]');
+    
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+    };
+
+    function hideModalByTime(selector, time) {
+        setTimeout(function() {
+            document.querySelector(selector).style.display = 'none';
+            document.body.style.overflow = "";
+        }, time);
+    }
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+
+            const formData = new FormData(item);
+            if (item.getAttribute('data-calc') === "end") {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
+
+            postData('assets/server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    statusMessage.textContent = message.success;
+                })
+                .catch(() => statusMessage.textContent = message.failure)
+                .finally(() => {
+                    clearInputs();
+                    hideModalByTime('.popup_engineer', 2500);
+                    hideModalByTime('.popup_calc_end', 2500);
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2500);
+                });
+        });
+    });
+};
+
+export default forms;
